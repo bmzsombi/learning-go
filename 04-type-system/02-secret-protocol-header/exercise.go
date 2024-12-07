daf7347aed7cbbd6ced37196f034bee6a120822f
bmzsombi <bmzsombi@gmail.com>
2024-12-07 23:39:53 +0100

    asd

--- 04-type-system/02-secret-protocol-header/exercise.go
+++ 04-type-system/02-secret-protocol-header/exercise.go
@@ -6,4 +6,30 @@ package secretprotocolheader
 // createPublishFixHeader constructs an octet (8-bit long byte) based on its three arguments and the fix QoS setting.
 func createPublishFixHeader(isFirstAttempt bool, isBroadcasted bool, isSecure bool) byte {
 	// INSERT YOUR CODE HERE
+	QoS := byte(0b10)
+
+	var header byte
+	header |= QoS << 2
+
+	if isFirstAttempt {
+		header |= 1 << 4
+	} else {
+		header |= 0 << 4
+	}
+
+	if isBroadcasted {
+		header |= 1 << 1
+	} else {
+		header |= 0 << 1
+	}
+
+	if isSecure {
+		header |= 1 << 0
+	} else {
+		header |= 0 << 0
+	}
+
+	header |= byte(0b010) << 5
+
+	return header
 }
