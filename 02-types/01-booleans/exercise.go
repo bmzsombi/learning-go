228ab8f0c5047b6957c19b1df62911a6ee0e3fef
bmzsombi <bmzsombi@gmail.com>
2024-12-07 22:08:54 +0100

    first 1,5 done

--- 02-types/01-booleans/exercise.go
+++ 02-types/01-booleans/exercise.go
@@ -4,3 +4,15 @@ package logicalops
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
 // INSERT YOUR CODE HERE
+func inverse(b bool) bool {
+	// INSERT YOUR CODE HERE
+	return !b
+}
+
+func and(x, y bool) bool {
+	return x && y
+}
+
+func deMorgan(a, b bool) bool {
+	return and(inverse(a), inverse(b))
+}
