f4411296c8c38c8aab508593c6cb4ec06cf83c59
bmzsombi <bmzsombi@gmail.com>
2024-12-08 17:15:30 +0100

    20 1/3 done

--- 20-goroutines/01-concurrent-primes/exercise.go
+++ 20-goroutines/01-concurrent-primes/exercise.go
@@ -1,6 +1,54 @@
 package concurrentprimes
 
+import "time"
+
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
 // INSERT YOUR CODE HERE
+func GeneratePrimes(n int) []int {
+	if n < 2 {
+		return []int{}
+	}
+
+	numChan := make(chan int)
+	primesChan := make(chan int)
+
+	go func() {
+		defer close(numChan)
+		for i := 2; i <= n; i++ {
+			numChan <- i
+		}
+	}()
+
+	go func() {
+		defer close(primesChan)
+		primesChan <- 2
+		primes := []int{2}
+
+		for num := range numChan {
+			isPrime := true
+			for _, p := range primes {
+				if num%p == 0 {
+					isPrime = false
+					break
+				}
+				if p*p > num {
+					break
+				}
+			}
+			if isPrime {
+				primes = append(primes, num)
+				primesChan <- num
+			}
+		}
+	}()
+
+	primes := []int{}
+	for prime := range primesChan {
+		primes = append(primes, prime)
+	}
+
+	time.Sleep(50 * time.Millisecond)
+	return primes
+}
--- 20-goroutines/02-concurrent-word-count/exercise.go
+++ 20-goroutines/02-concurrent-word-count/exercise.go
@@ -4,3 +4,22 @@ package wordcount
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
 // INSERT YOUR CODE HERE
+func CountWords(words []string) map[string]int {
+	if len(words) == 0 {
+		return map[string]int{}
+	}
+
+	var returnmap map[string]int
+	countchan := make(chan int)
+
+	for i, val1 := range words {
+		for j, val2 := range words {
+			go func(word string, returnmap map[string]int) {
+				defer close(countchan)
+				if words[i] == words[j] {
+					val1++
+				}
+			}(words[i], returnmap)
+		}
+	}
+}
