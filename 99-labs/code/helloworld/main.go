5cbba3e63f3c1e433fe73092626d4f7e886d3d09
bmzsombi <bmzsombi@gmail.com>
2025-01-11 14:16:03 +0100

    first lab done

---
+++ 99-labs/code/helloworld/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"runtime"
+)
+
+var hostname string
+var version string
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world from %s running Go version %s!\n", hostname, version)
+}
+
+func main() {
+	h, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	v := runtime.Version()
+	version = v
+	hostname = h
+	http.HandleFunc("/", HelloHandler)
+	http.ListenAndServe(":8080", nil)
+}
